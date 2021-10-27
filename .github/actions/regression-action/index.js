c76f8aa17eda027ab80f16f98c493395f557ff4f
Diego Costantino <diego@diegocostantino.com>
2021-10-27 10:14:44 -0700

    pass action input

--- .github/actions/regression-action/index.js
+++ .github/actions/regression-action/index.js
@@ -1,4 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const regressionLabel = core.getInput("regressionLabel");
+const regressionLabel = core.getInput('regressionLabel');
+const issue = core.getInput('issue');
+console.log(issue);
